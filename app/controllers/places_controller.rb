d39ae85e7b2517650f916f823d12c64f4a2c065e
AndyAbplanalp <freshtech@gmail.com>
2015-04-09 13:28:34 +0000

    Lesson 18

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,7 @@
 class PlacesController < ApplicationController
+
+	before_action :authenticate_user!, :only => [:new, :create]
+
 	def index
 		@places = Place.all.paginate(:page => params[:page], :per_page => 5)
 	end
@@ -8,12 +11,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+#		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 
 	private
-	
+
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
