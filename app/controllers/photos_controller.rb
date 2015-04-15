245845ba2cb37ca748af2d09e5a4dda353c1c807
AndyAbplanalp <freshtech@gmail.com>
2015-04-15 19:38:20 +0000

    first commit lessson 39

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,19 @@
+class PhotosController < ApplicationController
+	    before_action :authenticate_user!
+    
+    def create
+        @place = Place.find(params[ :place_id])
+        if @place.user == current_user
+           @place.photos.create(photo_params)
+            redirect_to place_path(@place) 
+        else
+            return render :text => "Not Allowed", :status => :forbidden
+        end
+    end
+    
+    private
+    
+    def photo_params
+        params.require( :photo).permit( :picture, :caption)
+    end
+end
